02df57ff519916798319cb6b420b7beb1aef0ca6
Visarm44 <118958222+Visarm44@users.noreply.github.com>
2025-11-26 11:20:02 +0100

    Added main text area with syntax highlighting

--- app/libraries.py
+++ app/libraries.py
@@ -1,3 +1,4 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header
-from textual.containters import VerticalScroll
+#from textual.containters import VerticalScroll
+from textual.widgets import TextArea
--- app/textual_class.py
+++ app/textual_class.py
@@ -1,11 +1,16 @@
 from libraries import *
 
 class TextEditor(App):
-
+    CSS = """
+        TextArea {
+            height: 1fr;
+        }
+    """
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         yield Header()
+        yield TextArea(language="markdown", id="main-editor")
         yield Footer()
 
     def action_toggle_dark(self) -> None:   # An action to toggle dark mode.
