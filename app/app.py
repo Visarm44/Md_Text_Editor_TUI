bf86096d07aa267f93b5b1504073b36fdda95f53
Visarm44 <118958222+Visarm44@users.noreply.github.com>
2025-11-26 11:04:16 +0100

    Added header and footer + theme swich

---
+++ app/app.py
@@ -0,0 +1,7 @@
+from libraries import *
+from textual_class import TextEditor
+
+
+if __name__ == "__main__":
+    app = TextEditor()
+    app.run()
---
+++ app/textual_class.py
@@ -0,0 +1,14 @@
+from libraries import *
+
+class TextEditor(App):
+
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+
+    def action_toggle_dark(self) -> None:   # An action to toggle dark mode.
+        self.theme = (
+            "textual-dark" if self.theme == "textual-light" else "textual-light"
+        )
